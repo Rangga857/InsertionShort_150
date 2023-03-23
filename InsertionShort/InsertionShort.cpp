e77b3c92fcf1e0e0bcbdfe174b0b694fca11ab9d
Rangga857 <azharrangga28@gmail.com>
2023-03-23 15:35:15 +0700

    menginput nilai data ke n

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -25,7 +25,12 @@ void input()
 	cout << endl;
 	cout << "======================" << endl;
 	cout << "Masukkan Element Array" << endl;
-	cout << "======================" << endl:
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Data ke-" << (i + 1) << ": ";
+	}
 
 
 
