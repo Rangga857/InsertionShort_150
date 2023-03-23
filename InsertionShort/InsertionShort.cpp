d897e61feaed8daad1e92430fd8f393bfd0c39bf
Rangga857 <azharrangga28@gmail.com>
2023-03-23 15:34:08 +0700

    menggunakan perulangan for untuk menyimpan data pada  array

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -10,5 +10,23 @@ void input()
 	{
 		cout << "Masukkan jumlah data pada Array : ";
 		cin >> n;
+
+		if (n <= 20)
+		{
+			break;
+
+		}
+		else
+		{
+			cout << "n\Array yang anda masukkan maksimal 20 elemen. \n";
+
+		}
 	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl:
+
+
+
 }
