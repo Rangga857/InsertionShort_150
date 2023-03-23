58432da7c36c3f64168de859b140a29bfab2dc0f
Rangga857 <azharrangga28@gmail.com>
2023-03-23 16:01:03 +0700

    memanggil display

--- InsertionShort/InsertionShort.cpp
+++ InsertionShort/InsertionShort.cpp
@@ -63,5 +63,25 @@ void insertionsort()
 		}
 	}
 }
+void display()
+{
+	cout << endl;
+	cout << "TOTAL ELEMENT MOVEMENT" << n - 1 << endl;
+	cout << "\======================" << endl;
+	cout << "ELEMENT ARRAY YANG TELAH TERSUSUN" << endl;
+	cout << "=======================" << endl;
 
+	for (int j = 0; j < n; j++)
+	{
+		cout << arr[j] << endl;
+
+
+	}
+	cout << endl;
+
+}
+int main()
+{
+	input();
+}
 
